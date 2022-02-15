06ff1b54aff5011bd733b662d503bd11c367a420
eahenle <eahenle@gmail.com>
2022-02-14 23:27:25 -0800

    hw5

---
+++ hw5_15-5.jl
@@ -0,0 +1,50 @@
+### A Pluto.jl notebook ###
+# v0.17.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a71eca2a-8e23-11ec-2c14-7580b8f43ec3
+md"## Problem 15-5"
+
+# ╔═╡ cb2d6283-088b-4adc-89dc-ae758558b420
+md"
+For the first-order irreversible reaction $A\rightarrow B$ in a spherical catalyst pellet, $C_A(R/2)=\frac{1}{10}C_{A,s}$.  $C_{A,s}=10^{-6}$ mol/cm³, $R=0.001$ cm, and $D=0.1$ cm²/s.
+"
+
+# ╔═╡ 7e3059d8-2a83-4df1-92ac-94c10582f726
+md"**(a)** What is the concentration of reactant at $r=3*10^{-4}$ cm?"
+
+# ╔═╡ 6f0a264f-62f5-4f8a-9e58-1dcb14a8bf57
+
+
+# ╔═╡ 012e6fa9-157d-4d1f-962d-db2e4b3f7650
+md"**(b)** What diameter of pellet should be used to obtain an effectiveness factor of $\eta=0.8$?"
+
+# ╔═╡ 8981f25e-d308-405d-a92c-a31c0e757ac3
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.0"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─a71eca2a-8e23-11ec-2c14-7580b8f43ec3
+# ╟─cb2d6283-088b-4adc-89dc-ae758558b420
+# ╟─7e3059d8-2a83-4df1-92ac-94c10582f726
+# ╠═6f0a264f-62f5-4f8a-9e58-1dcb14a8bf57
+# ╟─012e6fa9-157d-4d1f-962d-db2e4b3f7650
+# ╠═8981f25e-d308-405d-a92c-a31c0e757ac3
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
